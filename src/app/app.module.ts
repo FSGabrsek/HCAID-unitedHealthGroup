b7fcc22655ea78c12750d23c6c283b1e8df166c2
FSGabrsek <f.gabrsek@gmail.com>
2023-10-17 12:43:01 +0200

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHomeComponent } from './features/page-home/page-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
   ],
   imports: [
     BrowserModule,
