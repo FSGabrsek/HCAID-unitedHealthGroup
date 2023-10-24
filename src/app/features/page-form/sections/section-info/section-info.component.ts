ef054d4105c1793f845abd1745405ee353d147ac
FSGabrsek <f.gabrsek@gmail.com>
2023-10-24 17:08:39 +0200

    added form input persistence

--- src/app/features/page-form/sections/section-info/section-info.component.ts
+++ src/app/features/page-form/sections/section-info/section-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-section-info',
@@ -12,11 +13,17 @@ export class SectionInfoComponent {
     fbs!: number
     restecg!: number
 
+    constructor(private dataservice: DataService) {
+
+    }
+
     valid(): boolean {
         return ([1, 2, 3, 4].indexOf(this.cp) > -1) && (this.trestbps > 0) && (this.chol > 0) && ([1, 0].indexOf(this.fbs) > -1) && ([0, 1, 2].indexOf(this.restecg) > -1)
     }
 
     advance() {
-        
+        this.dataservice.save_info(this.cp, this.trestbps, this.chol, this.fbs, this.restecg)
+        console.log(this.dataservice.valid);
+
     }
 }
