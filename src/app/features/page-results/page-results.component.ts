1a340af7161b1c4bc2d0dfd13c0baec0510c9d99
FSGabrsek <f.gabrsek@gmail.com>
2023-11-01 16:28:34 +0100

    added premium calculation

--- src/app/features/page-results/page-results.component.ts
+++ src/app/features/page-results/page-results.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-results.component.scss']
 })
 export class PageResultsComponent {
-    premium = 250
+    base = 225
+    alpha = 1.0
+    prediction = 0
+    voting_confidence = 0
+
+    premium(): number {
+        return Math.round(this.alpha * ((this.prediction * (1 + this.voting_confidence) * this.base) + ((1 - this.prediction) * (2 - this.voting_confidence) * this.base)))
+    }
 }
