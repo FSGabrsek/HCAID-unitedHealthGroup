c8ae0e3332ffd4273ab50877d8d30d8c29900776
FSGabrsek <f.gabrsek@gmail.com>
2023-11-02 12:22:02 +0100

    reordered clear

--- src/app/features/page-disclaimer/page-disclaimer.component.ts
+++ src/app/features/page-disclaimer/page-disclaimer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-page-disclaimer',
@@ -8,4 +9,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class PageDisclaimerComponent {
     isChecked: boolean = false
+
+    constructor(
+        private dataService: DataService
+    ) {}
+
+    start() {
+        this.dataService.clear()
+    }
 }
