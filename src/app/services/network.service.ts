434e32bcc73be83fc8b3b35499755adeaaa70a53
FSGabrsek <f.gabrsek@gmail.com>
2023-11-02 17:26:30 +0100

    changed to https

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -6,7 +6,7 @@ import { form_input } from '../core/models/form-input';
   providedIn: 'root'
 })
 export class NetworkService {
-    APIUrl = 'http://diseaseheartpredictor.pythonanywhere.com'
+    APIUrl = 'https://diseaseheartpredictor.pythonanywhere.com'
 
     constructor(
         private http: HttpClient
